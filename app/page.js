e4c0151f5e549c12d805e574134bd7126a48bc26
STARTRAVEL\aqua5063 <aqua5063@startravel.com.tw>
2023-06-26 10:52:48 +0800

    first update

--- app/page.js
+++ app/page.js
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import { Canvas } from "@react-three/fiber";
 
-export default function Home() {
+import Box from "../components/Box";
+import Floor from "../components/Floor";
+import LightBulb from "../components/Light";
+import OrbitControls from "../components/OrbitControls";
+import Draggable from "../components/Draggable";
+import { Suspense } from "react";
+
+import styles from "./styles/page.module.css";
+
+export default function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className={styles.scene}>
+      <Canvas
+        shadows
+        className={styles.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color='red' intensity={0.3} />
+        <LightBulb position={[0, 3, 0]} />
+        <Draggable>
+          <Suspense fallback={null}>
+            <Box rotateX={3} rotateY={0.2} />
+          </Suspense>
+        </Draggable>
+        <Floor position={[0, -1, 0]} />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
