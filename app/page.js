cef4f3cd4df97f0c48dd11d1ab1641ab07cbc924
STARTRAVEL\aqua5063 <aqua5063@startravel.com.tw>
2023-07-21 17:05:23 +0800

    update params

--- app/page.js
+++ app/page.js
@@ -1,31 +1,37 @@
 "use client"
-import { Canvas } from "@react-three/fiber";
-
-import Box from "../components/Box";
-import Floor from "../components/Floor";
-import LightBulb from "../components/Light";
-import OrbitControls from "../components/OrbitControls";
-import Draggable from "../components/Draggable";
 import Model from "../components/Model"
+import { Canvas } from "@react-three/fiber";
+import { useState } from 'react'
+import OrbitControls from "../components/OrbitControls";
 import { Suspense } from "react";
 
 import styles from "./styles/page.module.css";
 
 export default function App() {
+  const [params, setParams] = useState('');
+
+  const handleReceivedParams = (params) => {
+    setParams(params);
+  };
+
   return (
-    <div className={styles.scene}>
-      <Canvas
-        shadows
-        className={styles.canvas}
-        camera={{ fov: 75, position: [0, 0, 3] }}
-      >
-        <color attach="background" args={['#f0f0f0']} />
+    <>
+      <div className={styles.scene}>
+        <Canvas
+          shadows
+          className={styles.canvas}
+          camera={{ fov: 75, position: [0, 0, 3] }}
+        >
           <Suspense fallback={null}>
-            <Model />
+            <Model onParamsReceived={handleReceivedParams}/>
           </Suspense>
 
-        <OrbitControls />
-      </Canvas>
-    </div>
+          <OrbitControls />
+        </Canvas>
+      </div>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px', color:'black' }} href="#" >
+      double click to enter portal {params}
+      </div>
+    </>
   );
 }
--- components/Model.js
+++ components/Model.js
@@ -1,6 +1,8 @@
-import React, { useRef } from "react";
+"use client"
+import { useEffect, useRef, useState } from 'react'
+import { useRouter, useParams } from 'next/navigation'
 import { useLoader, extend, useFrame } from "@react-three/fiber";
-import { MeshPortalMaterial, CameraControls, Gltf, Text } from '@react-three/drei'
+import { MeshPortalMaterial, CameraControls, Gltf, Text, useCursor } from '@react-three/drei'
 import { easing, geometry } from 'maath'
 import * as THREE from 'three'
 import { suspend } from 'suspend-react'
@@ -9,27 +11,37 @@ extend(geometry)
 const regular = import('@pmndrs/assets/fonts/inter_regular.woff')
 const medium = import('@pmndrs/assets/fonts/inter_medium.woff')
 
-function Box(props) {
+function Model({onParamsReceived }) {
+
+    const handleFrameDoubleClick = (id) => {
+        onParamsReceived(id);
+      };
+
     return (
         <>
-            <Frame id="01" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
+            <color attach="background" args={['#f0f0f0']} />
+            <Frame id="1" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]} onFrameDoubleClick={handleFrameDoubleClick} >
                 <Gltf src="/model/pickles_3d_version_of_hyuna_lees_illustration-transformed.glb" scale={8} position={[0, -0.7, -2]} />
             </Frame>
-            <Frame id="02" name="tea" author="Omar Faruq Tawsif">
+            <Frame id="2" name="tea" author="Omar Faruq Tawsif" onFrameDoubleClick={handleFrameDoubleClick} >
                 <Gltf src="/model/fiesta_tea-transformed.glb" position={[0, -2, -3]} />
             </Frame>
-            <Frame id="03" name="still" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
+            <Frame id="3" name="still" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]} onFrameDoubleClick={handleFrameDoubleClick} >
                 <Gltf src="/model/still_life_based_on_heathers_artwork-transformed.glb" scale={2} position={[0, -0.8, -4]} />
             </Frame>
         </>
     );
 }
+export default Model;
 
-export default Box;
-
-function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, children, ...props }) {
+function Frame({ id, name, author, bg, width = 1, height = 1.61803398875,  children, onFrameDoubleClick, ...props }) {
     const portal = useRef()
-    //useFrame((state, dt) => easing.damp(portal.current, 'blend', params?.id === id ? 1 : 0, 0.2, dt))
+    const router = useRouter()
+    const [hovered, hover] = useState(false)
+    let params = ""
+    console.log(params)
+    useCursor(hovered)
+    useFrame((state, dt) => easing.damp(portal.current, 'blend', params === id ? 1 : 0, 0.2, dt))
     return (
         <group {...props}>
             <Text font={suspend(medium).default} fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
@@ -41,9 +53,12 @@ function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, childr
             <Text font={suspend(regular).default} fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>
                 {author}
             </Text>
-            <mesh name={id}>
+            <mesh name={id} onDoubleClick={(e) => ( params = id, onFrameDoubleClick(id))} 
+                            onPointerOver={(e) => hover(true)} 
+                            // onPointerOut={() => hover(false)}
+                            >
                 <roundedPlaneGeometry args={[width, height, 0.1]} />
-                <MeshPortalMaterial side={THREE.DoubleSide}>
+                <MeshPortalMaterial ref={portal} side={THREE.DoubleSide}>
                     <color attach="background" args={[bg]} />
                     {children}
                 </MeshPortalMaterial>
@@ -51,3 +66,17 @@ function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, childr
         </group>
     )
 }
+
+function Rig({ position = new THREE.Vector3(0, 0, 2), focus = new THREE.Vector3(0, 0, 0) }) {
+    const { controls, scene } = useThree()
+    const [, params] = useRoute('/item/:id')
+    useEffect(() => {
+      const active = scene.getObjectByName(params?.id)
+      if (active) {
+        active.parent.localToWorld(position.set(0, 0.5, 0.25))
+        active.parent.localToWorld(focus.set(0, 0, -2))
+      }
+      controls?.setLookAt(...position.toArray(), ...focus.toArray(), true)
+    })
+    return <CameraControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+  }
